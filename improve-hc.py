561daa6827b1bf4eacc554562bf77400b9dd6541
Ankur Singh <ankursi@iitk.ac.in>
2017-05-26 00:22:55 +0530

    The RL recipe call hill-climbing method
    This script involves the implementation of hill-climbing method and also an improved version of it.

---
+++ improve-hc.py
@@ -0,0 +1,45 @@
+import gym
+import numpy as np
+
+def run_episode(env,parameters):
+	observation=env.reset()
+	totalreward=0
+
+	for _ in range(200):
+		env.render()
+		action=0 if np.matmul(parameters,observation)<0 else 1
+		observation,reward,done,info=env.step(action)
+
+		totalreward+=reward
+
+		if done:
+			break
+	return totalreward
+
+def train(submit):
+	env=gym.make('CartPole-v0')
+	episodes_per_update=5
+
+	noise_scaling=0.1
+
+	parameters=np.random.rand(4)*2 - 1 #initialize weights b/w -1 and 1
+
+	bestreward=0
+
+	for _ in range(10000):
+		newparams=parameters+(np.random.rand(4)*2 - 1)*noise_scaling
+		reward=0
+		for i in range(episodes_per_update):
+			run=run_episode(env,parameters)
+			reward+=run
+		
+		print(reward)
+
+		if reward>bestreward:
+			bestreward=reward
+			parameters=newparams
+			if reward==200:
+				break
+
+r=train(submit=False)
+print(r)
